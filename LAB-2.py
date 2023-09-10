b6438e3cd36929a4033a2973e437cc14d89dcfd0
Vinay2004Kasana <143097284+Vinay2004Kasana@users.noreply.github.com>
2023-09-10 10:46:02 +0530

    Add files via upload

---
+++ LAB-2.py
@@ -0,0 +1,97 @@
+#x=int(input("LENGTH"))
+#y=int(input("BREADTH"))
+#Area=x*y
+#print(Area)
+
+#x=int(input("LENGTH in feet"))
+#y=int(input("BREADTH in feet"))
+#x=x/10.76
+#y=y/10.76
+#Area=x*y
+#print(Area)
+
+#x=int(input())
+#y=int(input())
+##if x<0 and y<0:
+ #   print("Invalid Input")
+#elif y%x==0:
+ #   print("y is divisible by x")
+#else :
+ #   print("y is not divisible by x")
+
+#x=int(input())
+#if x%2==0:
+ #   print("even")
+#else:
+ #   print("odd")
+
+
+#x=int(input())
+#if 1<x<100:
+ #   Area=3.14*(x*x)
+  #  print(Area)
+#else:
+   # print("invalid")
+
+'''x=int(input("T in C"))
+#y=temperature in fahrenheit
+y=9/5*x+32
+print(y)
+x=int(input("T in C"))
+#z=temperature in kelvin
+z=x+273
+print(z)'''
+
+'''x=int(input("T in C"))
+#y=temperature in fahrenheit
+y=9/5*x+32
+if(x==y):
+    print(x or y)
+else:
+    print("invalid")'''
+
+
+'''x=int(input( "Year-"))
+if(x%4==0):
+    print("leap year")
+else:
+    print("not a leap year")'''
+
+
+'''p=int(input("PRINCIPAL-"))
+t=int(input("TIME-"))
+r=7.1
+si=(p*7.1*t)/(100*12)  #12 is for per annum interest rate
+if(p>500 and t>6):
+    print(si)
+else:
+    print('invalid')'''
+
+
+'''time=int(input("time in range 1<t<86400->"))
+hour=int(time/3600)
+x=hour*3600
+minute=int((time-x)/60)
+y=minute*60
+second=(time-x-y)
+if(1<time<86400):
+    print("time is converted into hour:minute:second-",hour,minute,second)
+else:
+    print('invalid')'''
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ LAB-3.py
@@ -0,0 +1,125 @@
+#x=int(input())
+#for x in range (x):
+ #   a,b= map(int,input.split())
+  #  print(a-b)
+
+
+#x=int(input())
+#y=int(input())
+#H=int(input())
+#Area=1/2*H*(x+y)
+#print (Area)
+
+#x=float(input("Weight in kg"))
+#y=float(input("height in m"))
+#BMI=x/(y*y)
+#print(BMI)
+#if (BMI<18):
+ #   print("underweight")
+#elif (18<BMI<23):
+ #   print("normal")
+#else:
+ #   print("overweight")
+
+
+
+#x=int(input())
+#y=int(input())
+#z=int(input())
+#if(x+y>z):
+ #   print("triangle")
+#else:
+ #   print("invalid")
+
+
+#xyz=int(input())
+#ones=xyz %10//1
+#tens=(xyz %100) // 10
+#hundreds=(xyz %1000)//100
+#Sum=ones+tens+hundreds
+#print(Sum)
+#if ((Sum)%3==0):
+ #   print("divisible by 3")
+#else:
+ #   print("not divisible by 3")
+
+'''xyzrt=int(input())
+x=(xyzrt%10)//1
+y=(xyzrt%100)//10
+z=(xyzrt%1000)//100
+r=(xyzrt%10000)//1000
+t=(xyzrt%100000)//10000
+if ( y==r and x==t ):
+   print("palindrome")
+else:
+   print("not in palindrome")'''
+
+
+'''x=int(input())
+y=int(input())
+A=y
+B=x
+print(A)
+print(B)'''
+
+'''x=int(input())
+y=int(input())
+z=int(input())
+t=int(input())
+com_1=complex(x,y)
+com_2=complex(z,t)
+print(com_1+com_2)
+print(com_1*com_2)'''
+
+
+'''x=int(input("Basic Salary"))
+HRA=20/100*x
+TA=5/100*x
+DA=10/100*x
+GROSS_SALARY=x+HRA+TA+DA
+print(GROSS_SALARY)'''
+
+
+'''x=int(input("Basic Salary per annumm"))
+HRA=20/100*x
+TA=5/100*x
+DA=10/100*x
+GROSS_SALARY=x+HRA+TA+DA
+if(GROSS_SALARY<300000):
+    print(GROSS_SALARY)
+elif(300000<GROSS_SALARY<1000000):
+    print(10/100*GROSS_SALARY)
+elif(1000000<GROSS_SALARY<2500000):
+    print(20/100*GROSS_SALARY)
+else:
+    print(30/100*GROSS_SALARY)'''
+
+
+
+'''x=int(input())
+y=int(input())
+z=int(input())
+if(x>y>z):
+    print(x)
+elif(z>y>x):
+    print(z)
+elif(y>z>x):
+    print(y)'''
+
+
+'''xyz=int(input())
+x=(xyz%10)//1
+y=(xyz%100)//10
+z=(xyz%1000)//100
+if((x)**3 + (y)**3 + (z)**3 ==xyz):
+    print("Armstrong")
+else:
+    print("not Armstrong")'''
+
+
+
+
+
+
+
+
