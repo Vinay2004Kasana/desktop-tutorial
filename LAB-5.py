dea21d50183a847e1a0476f86fb6a15a206faf68
Vinay2004Kasana <143097284+Vinay2004Kasana@users.noreply.github.com>
2023-09-23 18:56:34 +0530

    Add files via upload

---
+++ LAB-5.py
@@ -0,0 +1,77 @@
+'''N=int(input())
+i=0
+while 1<N<21:
+    if(i<21):
+     print(N,"*",i,"=",N*i)
+     i+=1'''
+
+
+
+'''N=int(input())
+i=1
+while 1<N<1000:
+    if(N%i==0):
+        N+=1
+        continue
+    else:
+        print('not divisible by N')
+        N+=1'''
+
+
+# n=int(input())
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+
+# n=int(input())
+# for i in range(n):
+#     for j in range(n-i):
+#         print(" ",end=" ")
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+    
+
+# n=int(input())
+# for i in range(n):
+#     for j in range(n-i):
+#          print("  ",end="")
+#     for j in range(2*i+1):
+#          print("*",end=" ")
+#     for j in range(n-i):
+#          print(" ",end="")
+         
+#     print()
+
+
+# N=int(input())
+# x=0
+# i=0
+# i_1=0
+# while(N!=-999):
+#     x=int(input())
+#     t=x%N
+#     if (t==0):
+#         i=i+1
+#     else:
+#         i_1=i_1+1
+# print(i)
+# print(i_1)
+
+
+
+# n=int(input())
+# for i in range(n):
+#      for j in range(n-i):
+#           print(" ",end="")
+#      for j in range(i+1):
+#           print("*",end=" ")
+#      for j in range(n-i):
+#           print(" ",end="")
+#      print()
+
+       
+        
+    
+
---
+++ LAB-6.py
@@ -0,0 +1,79 @@
+'''sentence=input()
+count=0
+i=0
+#x=len(sentence)
+Vowels="aieouAIEOU"
+for char in sentence:
+   if char in Vowels:
+     count+=1
+print(count)'''
+
+
+'''N=int(input())
+i=0
+for i in range (N):
+    sum=1/N
+    N=N-1
+    sum=sum+1/N
+    print(sum*((10)**9))'''
+
+'''N=int(input())
+fact=1
+i=0
+for i in range (N):
+    fact=fact*i
+    sum=1/(fact)
+    N=N-1
+    sum=sum+(1/(fact))
+    print(sum)'''
+
+# n=int(input())
+# x=int(input())
+# sum=0
+# a=1
+# for i in range(n):
+#     n=(x**a)/a
+#     sum=sum+n
+#     a+=1
+# print(sum)
+
+
+# n=int(input())
+# for i in range(n):
+#     sum=(1/2)**n
+#     sum=sum+1
+# print(sum)
+
+# n=int(input())
+# x=int(input())
+# fact=1
+# sum=1
+# i=0
+# for i in range(n):
+#     fact=fact*(2*n+1)
+#     a=((-1)**n)*((x)**(2*n+1))/(fact)
+#     sum=sum+a
+# print(sum)
+# #starting n with zero
+
+
+# n=int(input())
+# x=int(input())
+# fact=1
+# sum=0
+# i=1
+# for i in range(n):
+#     fact=fact*(2*n-2)
+#     a=((-1)**n-1)*((x)**(2*n-2)/(fact))
+#     sum=sum+a
+# print(sum)
+# #starting n with one
+
+
+
+
+
+
+
+
+
