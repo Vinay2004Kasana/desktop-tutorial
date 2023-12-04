eb2cc9401d323f306bb04da938e22315db47f45f
Vinay2004Kasana <143097284+Vinay2004Kasana@users.noreply.github.com>
2023-12-04 21:32:46 +0530

    Add files via upload

---
+++ LAB-7.py
@@ -0,0 +1,89 @@
+# i=65
+# for i in range(65,91):
+#     print(chr(i))
+
+# x=input()
+# count=0
+# for i in x:
+#   count+=1
+# print(count)
+
+
+
+
+# x=input()
+# # print(x.replace('','-'))
+# print(x.lower().replace('','-'))
+
+# x=str(input())
+# t=x[: :-1]
+# if x==t:
+#     print('palindrome')
+# else:
+#     print('not palindrome')
+
+
+
+
+# x=str(input())
+# alpha_count1=0
+# alpha_count2=0
+# d_count=0
+# sp_count=0
+# for char in x:
+#     if char.islower():
+#         alpha_count1+=1
+#     elif char.isupper():
+#         alpha_count2+=1
+#     elif char.isdigit():
+#         d_count+=1
+#     else:
+#         sp_count+=1
+
+# print(alpha_count1)
+# print(alpha_count2)
+# print(d_count)
+# print(sp_count)
+     
+ 
+# x=input()
+# print(x[: :-1])
+
+# x=str(input())
+# t=''
+# for i in x:
+#     if i not in t:
+#         t+=i
+# print(t)
+
+# x=str(input())
+# y=str(input())
+# count=0
+# if y in x:
+#     x=x.count(x)
+#     print('present')
+#     count+=1
+# else:
+#     print('not present')
+# print(count)
+
+x=str(input())
+for i in (65,92 and 97,123):
+    if(65<i<92 and 97<i<123):
+        print('Pangrams')
+    else:
+        print('Not Pangrams')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
